94839b8c79f5c29899b0f01da607d20a8057abe3
RaunakShrest <110533553+RaunakShrest@users.noreply.github.com>
2023-04-05 12:05:35 +0545

    created reactIncrement and decrement using state

---
+++ reactIncDec.js
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+//hook
+const App=()=>{
+const [number, setNumber] = useState(0);
+  const changeCount  =(opr)=> {
+    opr === 'dec' ? setNumber(number -1): setNumber(number+1)
+  }
+
+  return (
+    <div>
+      <h1>{number}</h1>
+      <button onClick={()=>changeCount('inc')}>incremennt</button>
+      <button onClick={()=>changeCount('dec')}>decrement</button>
+    </div>
+    )
+}
+export default App
